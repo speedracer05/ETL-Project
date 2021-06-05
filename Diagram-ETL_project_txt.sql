5588ceb220d36c40da27e2af653915c2beeb02d0
emansuetti <emansuetti@yahoo.com>
2021-06-04 20:47:24 -0700

    SQL database creation and schema diagrams added

---
+++ Diagram-ETL_project_txt.sql
@@ -0,0 +1,34 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/S8OCBH
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "covid_progression_California" (
+    "date" DATE   NOT NULL,
+    "state" VARCHAR(5)   NOT NULL,
+    "deaths" INT   NOT NULL,
+    "daily_hospitalization" INT   NOT NULL,
+    "icu_hospitalized" INT   NOT NULL,
+    "positive_cases_viral" INT   NOT NULL,
+    "positive_cases_increase" INT   NOT NULL,
+    "test_result_total" INT   NOT NULL,
+    "test_result_total_increase" INT   NOT NULL,
+    CONSTRAINT "pk_covid_progression_California" PRIMARY KEY (
+        "date","state"
+     )
+);
+
+CREATE TABLE "covid_progression_New_York" (
+    "date" DATE   NOT NULL,
+    "state" VARCHAR(5)   NOT NULL,
+    "deaths" INT   NOT NULL,
+    "daily_hospitalization" INT   NOT NULL,
+    "icu_hospitalized" INT   NOT NULL,
+    "positive_cases_increase" INT   NOT NULL,
+    "test_result_total" INT   NOT NULL,
+    "test_result_total_increase" INT   NOT NULL,
+    CONSTRAINT "pk_covid_progression_New_York" PRIMARY KEY (
+        "date","state"
+     )
+);
+
